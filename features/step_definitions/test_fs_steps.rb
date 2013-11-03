9ffa96c2fc9dd57bd3fe01f603241d59192fa78e
shiftky <shiftky@gmail.com>
2013-11-03 16:41:06 +0900

    シナリオファイルの編集

---
+++ features/step_definitions/test_fs_steps.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+
+Given /^: マウントポイントにいる$/ do
+  p "hoge"
+end
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,3 +5,5 @@ Before do
   @features_root = File.expand_path('../../../', __FILE__)
   @dirs = [@features_root]
 end
+
+
