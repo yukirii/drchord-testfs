cde0cb2a57cefe1b1fd1f0a79ff1d8ba3b4722b7
shiftky <shiftky@gmail.com>
2013-11-03 16:54:20 +0900

    ファイルシステム起動のひな形を作成

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,32 @@
 # encoding: utf-8
 require 'aruba/cucumber'
+require 'chukan'
+include Chukan
 
 Before do
   @features_root = File.expand_path('../../../', __FILE__)
   @dirs = [@features_root]
 end
 
+class FSManager
+  def initialize
+  end
 
+  def run
+    p "run"
+  end
+
+  def stop
+    p "stop"
+  end
+end
+
+fs_manager = FSManager.new
+fs_manager.run
+World do
+  fs_manager
+end
+
+at_exit do
+  fs_manager.stop
+end
